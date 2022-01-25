35645b1e285f9b1703eed0537cbe4a3eb030d30d
jcpcabanada <jcpcabanada@gmail.com>
2022-01-24 23:57:07 -0800

    7th

--- api/server.js
+++ api/server.js
@@ -3,6 +3,9 @@ const helmet = require("helmet");
 const cors = require("cors");
 const usersRouter = require('./users/users-router')
 const authRouter = require('./auth/auth-router')
+const session = require('express-session')
+const Store = require('connect-session-knex')(session)
+const knex = require('../data/db-config')
 
 
 /**
@@ -20,6 +23,25 @@ const authRouter = require('./auth/auth-router')
 
 const server = express();
 
+server.use(session({
+  name: 'chocolatechip',
+  secret: 'shh',
+  saveUninitialized: false,
+  resave: false,
+  store: new Store({
+    knex,
+    createTable: true,
+    clearInterval: 1000 * 60 * 10,
+    tablename: 'sessions',
+    sidfieldname: 'sid',
+  }),
+  cookie: {
+    maxAge: 1000 * 60 * 10,
+    secure: false,
+    httpOnly: false,
+    // sameSite: 'none'
+  }
+}))
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
